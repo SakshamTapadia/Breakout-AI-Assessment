d703c900b4b5238bc032e1edc8d9c1c4a022598c
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2024-11-18 17:08:43 +0530

    final.py

--- final.py
+++ final.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests
 import streamlit as st
-from serpapi.google_search_results import GoogleSearch
+from serpapi import GoogleSearch
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from gspread_dataframe import set_with_dataframe
